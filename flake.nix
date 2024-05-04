38efe4bb06ac4f90b83935ef5d9485ccdc52e052
Jakob Beckmann <f4z3r-github@pm.me>
2024-05-04 07:45:32 +0200

    fix: update nix shell for testing and better no_color support

--- flake.nix
+++ flake.nix
@@ -20,9 +20,46 @@
         luajit = pkgs.luajitPackages;
         default = pkgs.luajitPackages;
       };
+      custom-lua-packages = luaPkgs:
+        with luaPkgs; let
+          luatext = buildLuarocksPackage {
+            pname = "luatext";
+            version = "1.2.0-0";
+            knownRockspec =
+              (pkgs.fetchurl {
+                url = "mirror://luarocks/luatext-1.2.0-0.rockspec";
+                sha256 = "1lmjhsnbpz4wkaypqaqas0rlahbhpsr0g9wgls6lsswh3dh5xx28";
+              })
+              .outPath;
+            src = pkgs.fetchgit (removeAttrs (builtins.fromJSON ''              {
+                "url": "https://github.com/f4z3r/luatext.git",
+                "rev": "c8ada5144c7c23d44d37a10fa484fe91b34c11d0",
+                "date": "2024-03-26T18:27:25+01:00",
+                "path": "/nix/store/crga0pkvm25gwwns8n6jb9cakfkmjd90-luatext",
+                "sha256": "0h6l7ws6baq5dxfs1ala8smm7pykh9vd13bxkh2a343hn834dx42",
+                "hash": "sha256-gvRGBrJwkKEEnH2N0HaC099Tq0aKqqBdbwWrZTQ/1EA=",
+                "fetchLFS": false,
+                "fetchSubmodules": true,
+                "deepClone": false,
+                "leaveDotGit": false
+              }
+            '') ["date" "path" "sha256"]);
+
+            disabled = luaOlder "5.1";
+            propagatedBuildInputs = [lua];
+
+            meta = {
+              homepage = "https://github.com/f4z3r/luatext/tree/main";
+              description = "A small library to print colored text";
+              license.fullName = "MIT";
+            };
+          };
+        in [luatext];
       makeLuaShell = shellName: luaPackage:
         pkgs.mkShell {
-          packages = with luaPackage; [busted argparse lyaml compat53];
+          packages = with luaPackage;
+            [busted argparse lyaml compat53]
+            ++ (custom-lua-packages luaPackage);
           shellHook = ''
             export LUA_PATH="./?.lua;./?/init.lua;$LUA_PATH"
           '';
--- sofa/pickers/fzf.lua
+++ sofa/pickers/fzf.lua
@@ -29,10 +29,7 @@ end
 ---@param str string
 ---@param color number
 ---@return string
-function Fzf:colorize(str, color)
-  if self.no_color then
-    return str
-  end
+local function colorize(str, color)
   return text.Text:new(str):fg(color):render()
 end
 
@@ -94,18 +91,18 @@ end
 ---@param interactive boolean
 ---@return Command
 function Fzf:pick_command(namespaces, interactive)
-  local sep = self:colorize(COMMAND_SEPARATOR, text.Color.Magenta)
+  local sep = colorize(COMMAND_SEPARATOR, text.Color.Magenta)
   local choices = {}
   for ns_name, ns in pairs(namespaces) do
     for cmd_name, cmd in pairs(ns:get_commands(interactive)) do
       local choice = string.format("%-20s%s%s", ns_name, sep, cmd_name)
       if cmd.description then
-        local desc = self:colorize(string.format("(%s)", cmd.description), text.Color.Cyan)
+        local desc = colorize(string.format("(%s)", cmd.description), text.Color.Cyan)
         choice = string.format("%s %s", choice, desc)
       end
       local tags = table.concat(cmd.tags, ", ")
       if tags ~= "" then
-        tags = self:colorize(string.format("[%s]", tags), ANSI_GRAY)
+        tags = colorize(string.format("[%s]", tags), ANSI_GRAY)
         choice = string.format("%s %s", choice, tags)
       end
       choices[#choices + 1] = choice
@@ -133,7 +130,7 @@ function Fzf:_template_param_choice(choice, sub)
   if sub == choice then
     return choice
   end
-  return string.format("%s (%s)", choice, self:colorize(sub, text.Color.Green))
+  return string.format("%s (%s)", choice, colorize(sub, text.Color.Green))
 end
 
 ---returns markup defined choices for a parmaters based on `choices` field. The default, if any, will always be the
@@ -182,7 +179,7 @@ function Fzf:pick_parameter(parameter, command)
   if self.no_color then
     sub = string.format(">%s<", sub)
   else
-    sub = self:colorize(sub, text.Color.Red)
+    sub = colorize(sub, text.Color.Red)
   end
   command = command:gsub(string.format("{{%%s*%s%%s*}}", parameter.name), sub)
   local options = {
